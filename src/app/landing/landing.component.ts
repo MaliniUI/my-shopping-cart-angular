e26dec9a46d1d59e73fa204e0f4ba7ae8aa0f6c3
MaliniUI <malinibe2010@gmail.com>
2019-12-05 14:17:02 +0530

    3 files modified

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -12,6 +12,7 @@ export class LandingComponent implements OnInit {
   updateCartCount: number = 0;
 
    toBeInserted: Iitems[] = [];
+   tempItem: Iitems[] = [];
 
   constructor(private countUpdate: SharedService, private cartItemUpdated: SharedService) { }
 
@@ -20,13 +21,20 @@ export class LandingComponent implements OnInit {
     this.cartItemUpdated.currentCartItems.subscribe(item => this.toBeInserted = item);
   }
 
-itemToBeAddedToCart(data: any[]): void{
-    debugger;
-      this.toBeInserted.push(data);
-      this.updateCartCount = this.toBeInserted.length;
-       this.countUpdate.changeCount(this.updateCartCount);
-       this.cartItemUpdated.updateCartItems(this.toBeInserted);
-      console.log("cart items ==> "+this.toBeInserted);
+itemToBeAddedToCart(data: Iitems[]): void{
+      debugger;
+      if(null != data && undefined != data){
+        this.tempItem = this.toBeInserted.find(x => x.id == data.id);
+        if(this.tempItem != null && this.tempItem != undefined ){
+          this.tempItem.quantity = parseInt(this.tempItem.quantity) + 1;
+        }else{
+          this.toBeInserted.push(data);
+        }
+        this.updateCartCount = this.updateCartCount + 1;
+        this.countUpdate.changeCount(this.updateCartCount);
+        this.cartItemUpdated.updateCartItems(this.toBeInserted);
+        console.log("cart items ==> "+this.toBeInserted);
+      }       
     }
 
 }
