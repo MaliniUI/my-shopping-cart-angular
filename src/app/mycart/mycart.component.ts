805ade834320854ac4153c6cdc46a46879e305ff
MaliniUI <malinibe2010@gmail.com>
2019-12-06 13:59:55 +0530

    1 file modified

--- src/app/mycart/mycart.component.ts
+++ src/app/mycart/mycart.component.ts
@@ -11,18 +11,42 @@ export class MycartComponent implements OnInit {
 
   cartItemList: Iitems[] = [];
   totalAmount : number = 0;
+  updatedItem: number = 0;
+  curCount: number =0;
+  countChanged: number = 0;
 
-  constructor(private cartItems: SharedService) { }
+  constructor(private cartItems: SharedService, private currentCount: SharedService) { }
 
   ngOnInit() {
     this.cartItems.cartItems.subscribe(item => this.cartItemList = item);
+    this.currentCount.currentCount.subscribe(item => this.curCount = item);
 
      debugger;
     for(let item of this.cartItemList){ 
-      this.totalAmount += (parseInt(item.quantity) * parseInt(item.cost)) ;
+      this.totalAmount += (parseFloat(item.quantity) * parseInt(item.cost)) ;
     }
     console.log("Total ==> "+ this.totalAmount);
    
+  }
+
+  updateItemQuantity($event, id){
+    debugger;
+    this.totalAmount = 0;
+    this.countChanged = 0;
+    for(let r of this.cartItemList){
+     this.countChanged += parseInt(r.quantity);
+     this.totalAmount += parseInt(r.quantity) * parseInt(r.cost);
+    }
+    this.currentCount.changeCount(this.countChanged);
+  }
+
+  decreaseItemQuantity(obj){
+    debugger;
+
+  }
+
+  increaseItemQuantity(obj){
+    debugger;
   }
 
 
